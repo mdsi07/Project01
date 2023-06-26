abd267ec7f12b10962bba9eff10976722a238a27
mdsi07 <137636526+mdsi07@users.noreply.github.com>
2023-06-26 21:18:57 +0530

    Formatting Header Border and Changed Add Items to bold and GreenText

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("Hello... World");
+// console.log("Hello... World");
+
+var header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000';
+
+/* practices for list item formatting
+// var items = document.getElementsByClassName('list-group-item');
+// items[0].style.fontWeight = 'bold';
+// items[1].style.fontWeight = 'bold';
+// items[2].style.fontWeight = 'bold';
+// items[3].style.fontWeight = 'bold';
+
+// items[0].style.color = 'green';
+// items[1].style.color = 'green';
+// items[2].style.color = 'green';
+// items[3].style.color = 'green'; */
+
+
+var title = document.getElementsByClassName('title');
+title[0].style.fontWeight = 'bold';
+title[0].style.color = 'green';
