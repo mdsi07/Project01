cb8d66e377f9525e0dc6087492c70d64306aff7b
mdsi07 <137636526+mdsi07@users.noreply.github.com>
2023-06-26 22:02:14 +0530

    Changed bgColor of 3rd and List Elements to Bold

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // console.log("Hello... World");
 
+//TASK 03 Deliverables
+
 var header = document.getElementById('main-header');
 header.style.borderBottom = 'solid 3px #000';
 
@@ -19,3 +21,15 @@ header.style.borderBottom = 'solid 3px #000';
 var title = document.getElementsByClassName('title');
 title[0].style.fontWeight = 'bold';
 title[0].style.color = 'green';
+
+
+// TASK 04 Deliverables
+
+var items = document.getElementsByClassName('list-group-item');
+
+items[2].style.backgroundColor = 'green';
+
+items[0].style.fontWeight = 'bold';
+items[1].style.fontWeight = 'bold';
+items[2].style.fontWeight = 'bold';
+items[3].style.fontWeight = 'bold';
