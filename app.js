2e2d64ca0c6bb7d9af974f2b87af7084fbd0e26e
mdsi07 <137636526+mdsi07@users.noreply.github.com>
2023-06-28 17:27:58 +0530

    Added New List Element Without Same Class Name

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 
 //TASK 03 Deliverables
 
-var header = document.getElementById('main-header');
-header.style.borderBottom = 'solid 3px #000';
+//var header = document.getElementById('main-header');
+//header.style.borderBottom = 'solid 3px #000';
 
 /* practices for list item formatting
 // var items = document.getElementsByClassName('list-group-item');
@@ -18,18 +18,30 @@ header.style.borderBottom = 'solid 3px #000';
 // items[3].style.color = 'green'; */
 
 
-var title = document.getElementsByClassName('title');
-title[0].style.fontWeight = 'bold';
-title[0].style.color = 'green';
+//var title = document.getElementsByClassName('title');
+//title[0].style.fontWeight = 'bold';
+//title[0].style.color = 'green';
 
 
 // TASK 04 Deliverables
 
-var items = document.getElementsByClassName('list-group-item');
+//var items = document.getElementsByClassName('list-group-item');
 
-items[2].style.backgroundColor = 'green';
+//items[2].style.backgroundColor = 'green';
 
-items[0].style.fontWeight = 'bold';
-items[1].style.fontWeight = 'bold';
-items[2].style.fontWeight = 'bold';
-items[3].style.fontWeight = 'bold';
+//items[0].style.fontWeight = 'bold';
+//items[1].style.fontWeight = 'bold';
+//items[2].style.fontWeight = 'bold';
+//items[3].style.fontWeight = 'bold';
+
+
+// TASK 05 Deliverables
+
+var li = document.getElementsByTagName('li');
+
+
+//For background Color
+for(var i =0; i< li.length; i++)
+{
+    li[i].style.backgroundColor = '#f4f4f4';
+}
