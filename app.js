87de8c510dfb0313ba8f668b983b4704cd3ba810
mdsi07 <137636526+mdsi07@users.noreply.github.com>
2023-06-30 20:53:46 +0530

    Creating Nodes and Modifying Dom - TASK 07

--- app.js
+++ app.js
@@ -56,11 +56,81 @@
 // thirdItem.style.color = 'white';
 
 //using QuerySelectorAll()
-var secondItem = document.querySelectorAll('li');
-secondItem[1].style.color = 'green';
+// var secondItem = document.querySelectorAll('li');
+// secondItem[1].style.color = 'green';
 
-var odd = document.querySelectorAll('li:nth-child(odd)');
-for(var i = 0; i < odd.length; i++)
-{
-    odd[i].style.backgroundColor = 'green';
-}
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// for(var i = 0; i < odd.length; i++)
+// {
+//     odd[i].style.backgroundColor = 'green';
+// }
+
+
+
+//TASK 07 Deliverables
+
+var itemList = document.querySelector('#items');
+
+// (01) parentElement
+itemList.parentElement.style.backgroundColor = '#f1fff1';
+
+// (02) lastElementChild
+itemList.lastElementChild.textContent = 'List Element 4';
+
+// (03) lastChild
+console.log(itemList.lastChild);
+itemList.lastChild.textContent = 'End of the List Elements';
+
+// (04) createChild  <--
+console.log(itemList.child);
+
+// (05) firstElementChild
+itemList.firstElementChild.textContent = 'List Element 1';
+
+// (06) firstChild
+console.log(itemList.firstChild);
+itemList.firstChild.textContent = 'Belew are the List Elements';
+
+// (07) nextSibling <--
+console.log(itemList.nextSibling);
+
+// (08) nextelementsibling <--
+console.log(itemList.nextElementSibling);
+
+// (09) previousSibling <--
+console.log(itemList.previousSibling);
+
+// (10) previousElementSibling
+console.log(itemList.previousElementSibling);
+itemList.previousElementSibling.textContent = 'List Elements';
+
+// (11) createElement
+
+// create a div
+var  newDiv = document.createElement('div');
+
+// add a class
+newDiv.className = 'newDivClassName';
+
+// add a id
+newDiv.id = 'newDivId';
+
+// (12) add attribute
+newDiv.setAttribute('title', 'newDivTitle');
+
+// (13) create text node
+var newDivText = document.createTextNode('Welcome to');
+
+// (14) appendChild
+newDiv.appendChild(newDivText);
+
+// Now, Implement the Deleverables to the DOM
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+container.insertBefore(newDiv, h1);
+
+
+// I'm implement some changes for Page looking good
+itemList.children[1].textContent = 'List Element 2';
+itemList.children[2].textContent = 'List Element 3';
